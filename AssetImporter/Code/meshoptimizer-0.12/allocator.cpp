97f51f1c9fc2a2dfc99cc219edff29891b0cdace
DoerriesT <tim.doerries@gmx.de>
2020-02-26 00:39:55 +0100

    - removed files unnecessary for the GI assignment
    Former-commit-id: e265e9753b79ad63af24305fe35ed5cc407f2f57

--- AssetImporter/Code/meshoptimizer-0.12/allocator.cpp
+++
@@ -1,7 +0,0 @@
-#include "meshoptimizer.h"
-
-void meshopt_setAllocator(void* (*allocate)(size_t), void (*deallocate)(void*))
-{
-	meshopt_Allocator::Storage::allocate = allocate;
-	meshopt_Allocator::Storage::deallocate = deallocate;
-}
