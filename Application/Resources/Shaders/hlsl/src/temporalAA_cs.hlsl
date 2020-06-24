9dc5e4254d8ca9535a69d42518bf8d93e3b0b032
DoerriesT <tim.doerries@gmx.de>
2020-06-24 09:00:45 +0200

    - tweaked taa
    Former-commit-id: 66db6e7787ed7c300b21cedf620b845a6713a4da

--- Application/Resources/Shaders/hlsl/src/temporalAA_cs.hlsl
+++ Application/Resources/Shaders/hlsl/src/temporalAA_cs.hlsl
@@ -1,5 +1,6 @@
 #include "bindingHelper.hlsli"
 #include "temporalAA.hlsli"
+#include "commonFilter.hlsli"
 
 RWTexture2D<float4> g_ResultImage : REGISTER_UAV(RESULT_IMAGE_BINDING, RESULT_IMAGE_SET);
 Texture2D<float4> g_InputImage : REGISTER_SRV(INPUT_IMAGE_BINDING, INPUT_IMAGE_SET);
@@ -14,6 +15,7 @@ PUSH_CONSTS(PushConsts, g_PushConsts);
 
 float3 rgbToYcocg(float3 color)
 {
+	return color;
 	float3 result;
 	result.x = dot(color, float3(  1, 2,  1 ) );
 	result.y = dot(color, float3(  2, 0, -2 ) );
@@ -24,6 +26,7 @@ float3 rgbToYcocg(float3 color)
 
 float3 ycocgToRgb(float3 color )
 {
+	return color;
 	float y  = color.x * 0.25;
 	float co = color.y * 0.25;
 	float cg = color.z * 0.25;
@@ -68,31 +71,6 @@ float3 sampleHistory(float2 texCoord, float4 rtMetrics)
 	return filtered.rgb * (1.0 / filtered.a);
 }
 
-float3 clipAABB(float3 p, float3 aabbMin, float3 aabbMax)
-{
-    //Clips towards AABB center for better perfomance
-    float3 center   = 0.5 * (aabbMax + aabbMin);
-    float3 halfSize = 0.5 * (aabbMax - aabbMin) + 1e-5;
-    //Relative position from the center
-    float3 clip     = p - center;
-    //Normalize relative position
-    float3 unit     = clip / halfSize;
-    float3 absUnit  = abs(unit);
-    float maxUnit = max(absUnit.x, max(absUnit.y, absUnit.z));
-	
-	return (maxUnit > 1.0) ? clip * (1.0 / maxUnit) + center : p;
-}
-
-float2 weightedLerpFactors(float weightA, float weightB, float blend)
-{
-	float blendA = (1.0 - blend) * weightA;
-	float blendB = blend * weightB;
-	float invBlend = 1.0 / (blendA + blendB);
-	blendA *= invBlend;
-	blendB *= invBlend;
-	return float2(blendA, blendB);
-}
-
 [numthreads(8, 8, 1)]
 void main(uint3 threadID : SV_DispatchThreadID)
 {
@@ -125,29 +103,52 @@ void main(uint3 threadID : SV_DispatchThreadID)
 	
 	float2 velocity = g_VelocityImage.Load(int3(threadID.xy + velocityCoordOffset, 0)).xy;
 	
-	float3 currentColor = rgbToYcocg(g_InputImage.Load(int3(threadID.xy, 0)).rgb);
+	float3 currentColor = rgbToYcocg(simpleTonemap(g_InputImage.Load(int3(threadID.xy, 0)).rgb));
 	float3 neighborhoodMin = currentColor;
 	float3 neighborhoodMax = currentColor;
 	float3 neighborhoodMinPlus = currentColor;
 	float3 neighborhoodMaxPlus = currentColor;
 	
-	// sample neighborhood
 	{
-		for (int y = -1; y < 2; ++y)
+		float3 m1 = 0.0;
+		float3 m2 = 0.0;
+		for (int y = 0; y < 3; ++y)
 		{
-			for (int x = -1; x < 2; ++x)
+			for (int x = 0; x < 3; ++x)
 			{
-				float3 tap = rgbToYcocg(g_InputImage.Load(int3(threadID.xy + int2(x,y), 0)).rgb);
-				neighborhoodMin = min(tap, neighborhoodMin);
-				neighborhoodMax = max(tap, neighborhoodMax);
-				neighborhoodMinPlus = (x == 0 || y == 0) ? min(tap, neighborhoodMinPlus) : neighborhoodMinPlus;
-				neighborhoodMaxPlus = (x == 0 || y == 0) ? max(tap, neighborhoodMaxPlus) : neighborhoodMaxPlus;
+				float3 tap = rgbToYcocg(simpleTonemap(g_InputImage.Load(int3(threadID.xy + int2(x, y) - 1, 0)).rgb));
+				
+				m1 += tap;
+				m2 += tap * tap;
 			}
-		}
+		}	
+		
+		float3 mean = m1 * (1.0 / 9.0);
+		float3 stddev = sqrt(max((m2  * (1.0 / 9.0) - mean * mean), 1e-7));
+		
+		float wideningFactor = 1.0;
+		
+		neighborhoodMin = -stddev * wideningFactor + mean;
+		neighborhoodMax = stddev * wideningFactor + mean;
 	}
 	
-	neighborhoodMin = lerp(neighborhoodMin, neighborhoodMinPlus, 0.5);
-	neighborhoodMax = lerp(neighborhoodMax, neighborhoodMaxPlus, 0.5);
+	//// sample neighborhood
+	//{
+	//	for (int y = -1; y < 2; ++y)
+	//	{
+	//		for (int x = -1; x < 2; ++x)
+	//		{
+	//			float3 tap = rgbToYcocg(simpleTonemap(g_InputImage.Load(int3(threadID.xy + int2(x,y), 0)).rgb));
+	//			neighborhoodMin = min(tap, neighborhoodMin);
+	//			neighborhoodMax = max(tap, neighborhoodMax);
+	//			neighborhoodMinPlus = (x == 0 || y == 0) ? min(tap, neighborhoodMinPlus) : neighborhoodMinPlus;
+	//			neighborhoodMaxPlus = (x == 0 || y == 0) ? max(tap, neighborhoodMaxPlus) : neighborhoodMaxPlus;
+	//		}
+	//	}
+	//}
+	//
+	//neighborhoodMin = lerp(neighborhoodMin, neighborhoodMinPlus, 0.5);
+	//neighborhoodMax = lerp(neighborhoodMax, neighborhoodMaxPlus, 0.5);
 	
 	uint2 imageDims;
 	g_InputImage.GetDimensions(imageDims.x, imageDims.y);
@@ -159,7 +160,7 @@ void main(uint3 threadID : SV_DispatchThreadID)
 	// history is pre-exposed -> convert from previous frame exposure to current frame exposure
 	float exposureConversionFactor = asfloat(g_ExposureData.Load(1 << 2)); // 0 = current frame exposure | 1 = previous frame to current frame exposure
 	
-	float3 historyColor = rgbToYcocg(max(sampleHistory(prevTexCoord, float4(texSize, texelSize)).rgb, 0.0) * exposureConversionFactor);
+	float3 historyColor = rgbToYcocg(simpleTonemap(max(sampleHistory(prevTexCoord, float4(texSize, texelSize)).rgb, 0.0) * exposureConversionFactor));
 	historyColor = clipAABB(historyColor, neighborhoodMin, neighborhoodMax);
 	
 	float subpixelCorrection = abs(frac(max(abs(prevTexCoord.x) * texSize.x, abs(prevTexCoord.y) * texSize.y)) * 2.0 - 1.0);
@@ -170,7 +171,7 @@ void main(uint3 threadID : SV_DispatchThreadID)
 	
 	float2 factors = weightedLerpFactors(1.0 / (historyColor.x + 4.0), 1.0 / (currentColor.x + 4.0), alpha);
 	
-	float3 result = historyColor * factors.x + currentColor * factors.y;
+	float3 result = lerp(historyColor, currentColor, alpha);//historyColor * factors.x + currentColor * factors.y;
 	
-	g_ResultImage[threadID.xy] = float4(ycocgToRgb(result), 1.0);
+	g_ResultImage[threadID.xy] = float4(inverseSimpleTonemap(ycocgToRgb(result)), 1.0);
 }
