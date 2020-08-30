1383bbf9f39fd6a9b32496272a81aee9f24267ef
DoerriesT <tim.doerries@gmx.de>
2020-08-30 18:03:28 +0200

    - slightly increased raster tiling proxy meshes to fix some visual artifacts
    Former-commit-id: 9c99e1bf29f96839d15e004d51e06bae84a78ff4

--- Application/Resources/Shaders/hlsl/src/rasterTiling_vs.hlsl
+++ Application/Resources/Shaders/hlsl/src/rasterTiling_vs.hlsl
@@ -5,5 +5,5 @@ PUSH_CONSTS(PushConsts, g_PushConsts);
 
 float4 main(float3 position : POSITION) : SV_Position
 {
-	return mul(g_PushConsts.transform, float4(position, 1.0));
+	return mul(g_PushConsts.transform, float4(position * 1.1, 1.0));
 }
